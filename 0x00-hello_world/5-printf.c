d48ab2763627c2b1243ca9425b10850ca6dda228
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 00:08:37 +0300

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main( );
+/*Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.*/
+
+int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art");
-		return 0;
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
