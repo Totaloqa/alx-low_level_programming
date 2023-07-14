fbda20e2715527aa8e0243ca8e0fc2f348c3e509
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 22:57:21 +0300

    1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '10'; number++)
+	{
+		printf("number\n")
+	}
+
+	return (0);
+}
