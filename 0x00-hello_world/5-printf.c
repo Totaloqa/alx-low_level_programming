1de6e234abdb95e903aa6cb0f37a87d6b3c5f8a7
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 13:36:11 +0300

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/*Write a C program that uses printf*/
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0 if program executes successfully otherwise a non-zero value.
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
