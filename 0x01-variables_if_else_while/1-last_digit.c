60a1e4d6fb9aae978fdb382b0493e45d1ef6ae51
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 20:36:10 +0300

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,13 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
 	}
-	else if (y >= 1 && y <= 5)
+	else if (y > 6 && y != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	}
 	else if (y == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, y);
+		printf("Last digit of %d is %d and is 0\n", n, y);
 	}
 	return (0);
 }
