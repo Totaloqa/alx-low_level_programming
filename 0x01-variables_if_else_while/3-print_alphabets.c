b1c5ebe8c56f4f12d4725f2e13e206df35b44803
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 22:34:39 +0300

    1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: 0 always (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++);
+	{
+		putchar(letter);
+	}
+
+	putchar(\n);
+
+	for (letter = 'A'; letter <= 'Z'; letter++);
+	{
+		putchar(letters);
+	}
+
+	return (0);
+}
