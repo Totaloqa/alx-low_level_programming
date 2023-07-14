4fa8de88afb9304c5afea93850fdaf29d052fa5a
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 22:16:15 +0300

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char = letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
