bdc1a833690a182d8104af3506fdb1b8387ce43c
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-14 23:19:52 +0300

    1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-        int number;
+	int number;
 
-        for (number = '0'; number <= '9'; number++)
-        {
-                putchar(number);
-        }
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
