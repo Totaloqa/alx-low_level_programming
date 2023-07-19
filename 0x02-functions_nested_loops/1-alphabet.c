6350df6e741b99b0b191d12cfa12501a4c4918ab
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-19 21:49:33 +0300

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,14 +8,14 @@
 
 void print_alphabet(void)
 {
-	char letter:
+        char letter:
 
-		for (letter < 'a'; letter <= 'z'; letter++)
-		{
-			putchar(letter);
-		}
+                for (letter < 'a'; letter <= 'z'; letter++)
+                {
+                        putchar(letter);
+                }
 
-	putchar('\n');
+        putchar('\n');
 
-	return (0);
+        return (0);
 }
