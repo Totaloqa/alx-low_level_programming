0619c9996174144605142742625e027b9744059e
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-19 21:30:15 +0300

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *
+ */
+
+int main(void)
+{
+	char letter:
+
+		for (letter < 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+		}
+
+	putchar('\n');
+
+	return (0);
+}
