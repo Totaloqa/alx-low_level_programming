64082b69170e50e7c69cd543b87d5943f936ff11
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-20 17:37:41 +0300

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,9 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char mssg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar('mssg');
 
 	putchar('\n');
 
