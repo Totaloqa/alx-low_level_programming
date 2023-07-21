82f545b0d11e937764ad221a94e1cd48dbb73c1f
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-21 17:31:58 +0300

    1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 intergers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: always 0 (success)
+ */
+
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+	int c;
+
+	c = a * b;
+
+	putchar(c);
+}
