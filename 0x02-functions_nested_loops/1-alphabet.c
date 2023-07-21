33a35c2fe86618541c7d2a749d95840791bf18a4
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-21 16:07:30 +0300

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,20 +2,18 @@
 #include "main.h"
 
 /**
- * main - entry point
- * print_alphabet - Prints alphabet in lowercase
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: always 0 (success)
  */
 
 void print_alphabet(void)
 {
-        char letter:
-
-                for (letter < 'a'; letter <= 'z'; letter++)
-                {
-                        putchar(letter);
-                }
-
-        putchar('\n');
-
-        return (0);
+	char a:
+		for (a < 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+	putchar('\n');
+	return (0);
 }
