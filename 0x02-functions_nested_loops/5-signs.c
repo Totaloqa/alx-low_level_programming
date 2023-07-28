34d7f070e8cf0dd260b587f19d9ff901fe926f05
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-28 23:17:41 +0300

    1

---
+++ 0x02-functions_nested_loops/5-signs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints signs of a number
+ *
+ * @n: character to check
+ *
+ * Return: 1 if n is > 0, 0 if n = 0, -1 if n < 0
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		putchar('+');
+
+		return (1);
+	}
+	else if (n < '0')
+	{
+		putchar('-');
+
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+
+		return (0);
+	}
+}
