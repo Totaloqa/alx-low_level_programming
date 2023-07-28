b627d5178956f8c207212d2523ab548c7c4440de
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-07-28 18:35:40 +0300

    1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ */
+
+void _puts(char *str)
+{
+	char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	_puts(str);
+}
