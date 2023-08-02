dea89663d0f8d96162190a2865c1920f6013e286
Totaloqa <root@DESKTOP-0VDP2TN.localdomain>
2023-08-02 17:16:06 +0300

    1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ *
+ */
+
+int print_last_digit(int);
+{
+	int last_digit = n % 10;
+	putchar(last_digit);
+}
